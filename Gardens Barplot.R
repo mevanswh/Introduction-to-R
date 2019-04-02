2132fe3716dc1755342094cd93bb1b9660071019
bbhowardparker <46463149+bbhowardparker@users.noreply.github.com>
2019-04-02 17:43:32 -0500

    Add files via upload

---
+++ Gardens Barplot.R
@@ -0,0 +1,39 @@
+#I recreated the gardens chart here because I couldn't get it read into R for some reason
+gardenA<-c(3,4,4,3,2,3,1,3,5,2)
+gardenB<-c(5,5,6,7,4,4,3,5,6,5)
+gardenC<-c(3,3,2,1,10,4,3,11,3,10)
+gardens<-cbind(gardenA,gardenB,gardenC)
+gardens<-data.frame(gardens)
+#calculate means for each column
+avgmeanA<-mean(gardens$gardenA)
+avgmeanB<-mean(gardens$gardenB)
+avgmeanC<-mean(gardens$gardenC)
+#calculate standard deviations for each column
+devA<-sd(gardens$gardenA)
+devB<-sd(gardens$gardenB)
+devC<-sd(gardens$gardenC)
+#calculate standard error for each column
+stderrA<-devA/(sqrt(length(devA)))
+stderrB<-devB/(sqrt(length(devB)))
+stderrC<-devC/(sqrt(length(devC)))
+#create a vector of the means
+meanvec<-c(avgmeanA,avgmeanB,avgmeanC)
+#create a vector of the standard errors
+sevec<-c(stderrA,stderrB,stderrC)
+#can use the basic function 'barplot' in R to create a bar plot.
+#bind mean and standard error vectors to form a matrix 
+barmatrix<-cbind(meanvec,sevec)
+#now make a barplot using the matrix containing the means and standard errors
+#note that you can create the barplot using the vectors. If you use a 'matrix' object, you cannot use the '$' to call up the means column. You must use the following notation
+barplot(barmatrix[,1])
+#basic 'barplot' command can take a lot of arguments for aesthetics
+barplot((barmatrix[,1]),main = "Title of Bar Graph",ylab = "Y Axis Label",xlab = "X Axis Label",names.arg = c("A","B","C"),col="green")
+#arguments include the ability to make horizontal bars
+barplot(barmatrix[,1],main = "Title of Bar Graph", ylab = "Y Axis Label", xlab = "X Axis Label",names.arg = c("A","B","C"),col="green",horiz = TRUE)
+#read more about 'barplot' arguments with '?barplot'
+#replot with vertical bars and give the graph a name to fix it to the environment
+bplot<-barplot(barmatrix[,1],main = "Title of Bar Graph", ylab = "Y Axis Label", xlab = "X Axis Label",names.arg = c("A","B","C"),col="green")
+#now you can add the error bars. Notice that the arguments call up the name of the barplot to put the bars onto, then it uses the original vectors to actually create the positive and negative ends of the bars.
+segments(bplot,meanvec-sevec,bplot,meanvec+sevec)
+
+  

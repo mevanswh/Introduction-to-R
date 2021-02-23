b3efc41835d3856efb0e02ac9e8e8544b36e45b6
Michelle Evans-White <mevanswh@gmail.com>
2021-02-23 11:21:56 -0600

    Update Barplots with R basic and ggplot.R

--- Barplots with R basic and ggplot.R
+++ Barplots with R basic and ggplot.R
@@ -1,4 +1,4 @@
-#I recreated the gardens chart here because I couldn't get it read into R for some reason
+#In this lesson, we will learn how to make graphs using R.
 gardenA<-c(3,4,4,3,2,3,1,3,5,2)
 gardenB<-c(5,5,6,7,4,4,3,5,6,5)
 gardenC<-c(3,3,2,1,10,4,3,11,3,10)
